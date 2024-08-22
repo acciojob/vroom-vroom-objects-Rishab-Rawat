04f8de174645ee8819e1d24e22f99ad5ff9b8bcf
Rishab-Rawat <rawatrishab66@gmail.com>
2024-08-22 18:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 // Complete the js code
 function Car(make, model) {
-    this.make = make;
-    this.model = model;
+  this.make = make;
+  this.model = model;
 }
 
 Car.prototype.getMakeModel = function() {
-    return ${this.make} ${this.model};
+  return `${this.make} ${this.model}`;
 };
-
 function SportsCar(make, model, topSpeed) {
-    Car.call(this, make, model);
-    this.topSpeed = topSpeed;
+  // Call the Car constructor with make and model
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
 }
 
+// Inherit the Car prototype
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
 SportsCar.prototype.getTopSpeed = function() {
-    return this.topSpeed;
+  return this.topSpeed;
 };
-
-window.Car = Car;
-window.SportsCar = SportsCar;
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed());
